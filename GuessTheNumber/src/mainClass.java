1d8d1a15450b1bfbe1cac620589383b6985595d0
Ketan <k6890920@gmail.com>
2021-07-13 10:08:28 -0400

    Guess the number java

---
+++ GuessTheNumber/src/mainClass.java
@@ -0,0 +1,81 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class mainClass {
+    public static void main(String [] args) {
+        String c = " ";
+        int rand = (int)(Math.random() * 21);
+        int ctr = 1;
+        String name = "";
+        System.out.println("Hello! What is your name?");
+        Scanner in = new Scanner(System.in);
+        try {
+            name = in.nextLine();
+        }catch (Exception e){
+            System.out.println("Invalid input.");
+            return;
+        }
+        System.out.println();
+        System.out.println("Well, " + name + ", I am thinking of a number between 1 and 20.\n" +
+                "Take a guess.");
+
+
+           // for (int n = 1; n <= 6; n++) {
+        do {    Scanner i = new Scanner(System.in);
+            int guess = 0;
+              try{  guess = i.nextInt();
+              }catch (Exception e){
+                  System.out.println("Invalid input.");
+                  return;
+              }
+                if (guess > rand) {
+                    System.out.println("Your guess is too high.\n" +
+                            "Take a guess.");
+                } else if (guess < rand) {
+                    System.out.println("Your guess is too low.\n" +
+                            "Take a guess.");
+                } else if (guess == rand) {
+                    System.out.println("Good job, " + name + "! You guessed my number in " + ctr + " guesses!\n" +
+                            "Would you like to play again? (y or n)");
+                    Scanner input = new Scanner(System.in);
+                   try{ c = input.next();
+                   }catch (Exception e){
+                       System.out.println("Invalid input.");
+                       return;
+                   }
+                    if (c.equals("y")) {
+                        ctr = 0;
+                        System.out.println("Take a guess!");
+                    }
+                    else{
+                        break;
+                    }
+                }
+
+            ctr++;
+            if(ctr > 6) {
+                System.out.println("Better luck next time!");
+                System.out.println("Would you like to play again? (y or n)");
+                Scanner input = new Scanner(System.in);
+                String str = "";
+                try{ str = input.next();
+                }catch (Exception e){
+                    System.out.println("Invalid input.");
+                    return;
+                }
+                if (str.equals("y")) {
+                    ctr = 1;
+                    System.out.println("Take a guess!");
+                }
+                else{
+                    break;
+                }
+
+            }
+           // }
+        }while(ctr <= 6);
+
+
+    }
+
+}
